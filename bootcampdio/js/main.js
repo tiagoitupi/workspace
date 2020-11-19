a65485f3fa93f9511dda0c0bb4fb1cbdcd9cfef4
tiagoitupi <tiagoitupi@gmail.com>
2020-11-19 19:44:01 -0300

    Adicionado projeto de JS

---
+++ bootcampdio/js/main.js
@@ -0,0 +1,39 @@
+
+/* var nome = "Tiago dos santos";
+var idade = 20;
+var idade2 = 10;
+var frase = 'Japão é o melhor time do mundo';
+// alert("Bem vindo" + nome);
+
+//alert(idade+idade2);
+
+console.log(nome);
+console.log(idade2+idad);
+
+console.log(frase.replace('Japão','Brasil'));
+alert(frase.replace('Japão','Brasil'));
+
+----------
+
+var lista = ['Uva', 'Maçã', 'Melancia', 'Goiaba'];
+console.log(lista);
+lista.push('melão');
+lista.pop();
+console.log(lista.join("-"))
+
+alert(lista.length);
+
+-----------
+
+var fruta = {nome:"maçã", cor:"azul"};
+console.log(fruta.cor);
+
+-----------
+
+var frutas = [{nome: "maçã", cor:"Azul"}, {nome:"Uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+var divLocal = document.getElementById('nome');
+divLocal.innerHTML += "Tiago";
